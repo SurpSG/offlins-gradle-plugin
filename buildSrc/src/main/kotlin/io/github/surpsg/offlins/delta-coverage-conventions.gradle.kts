0a9cfc9ddf8aa7691caa14430c9c85a0ee59aa63
Sergii Gnatiuk <surpsg2012@gmail.com>
2025-01-07 21:53:09 +0200

    update CI

--- buildSrc/src/main/kotlin/io/github/surpsg/offlins/delta-coverage-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/io/github/surpsg/offlins/delta-coverage-conventions.gradle.kts
@@ -4,16 +4,10 @@ plugins {
     id("io.github.surpsg.delta-coverage")
 }
 
-val isGithub = project.hasProperty("github")
-
 deltaCoverageReport {
     diffSource.byGit {
         diffBase = project.properties["diffBase"]?.toString() ?: "refs/remotes/origin/main"
         useNativeGit = true
-    }
-
-    if (isGithub) {
-        coverageBinaryFiles = fileTree("tests-artifacts/jacoco") { include("**/*.exec") }
     }
 
     reports {
